b5f8d7ae6ff98bb18f30ed7dd7cb7c70263ce7ba
Igor Rzegocki <igor@rzegocki.pl>
2019-02-19 11:19:06 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,11 @@
+# ajgon/dot-os-linux
+Just a bunch of os-linux dotfiles.
+
+## Install
+Clone and symlink or install with [ellipsis][ellipsis]:
+
+```
+$ ellipsis install ajgon/dot-os-linux
+```
+
+[ellipsis]: http://ellipsis.sh
---
+++ ellipsis.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+config_home=${XDG_CONFIG_HOME:-$ELLIPSIS_HOME/.config}
+
+pkg.install() {
+    packages=(extensions git tmux vim zsh)
+    for package in ${packages[@]}; do
+        ellipsis install "ajgon/$package"
+    done
+}
+
+pkg.link() {
+    # Link package into ~/.config/git
+    if [ ! -d "${config_home}" ]; then
+        mkdir -p "${config_home}"
+    fi
+
+    if [ ! -d "${ELLIPSIS_HOME}/.local/share" ]; then
+        mkdir -p "${ELLIPSIS_HOME}/.local/share"
+    fi
+
+    fs.link_file fluxbox
+    fs.link_file "${PKG_PATH}/parcellite" "${config_home}/parcellite"
+    fs.link_file "${PKG_PATH}/xorg/xinitrc"
+    fs.link_file "${PKG_PATH}/xorg/Xresources"
+    fs.link_file "${PKG_PATH}/local/share/fonts" "${ELLIPSIS_HOME}/.local/share/fonts"
+}
+
+pkg.uninstall() {
+    : # No action
+}
